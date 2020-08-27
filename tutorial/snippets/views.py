88519c51a7c24f09b37aea0818bcc11d6e18433f
salujaharkirat <saluja.harkirat@gmail.com>
2020-08-27 22:30:09 +0530

    Step 1

---
+++ tutorial/snippets/views.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+# Create your views here.
+
+@csrf_exempt
+def snippets_list(request):
+  if request.method == "GET":
+    snippets = Snippet.objects.all()
+    serializer = SnippetSerializer(snippets, many=True)
+    return JsonResponse(serializer.data, safe=False)
+  
+  if request.method == "POST":
+    data = JSONParser.parse(request)
+    serializer = SnippetSerializer(data=data)
+    if serializer.is_valid():
+      serializer.save()
+      return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.data, status=400)
+
+@csrf_exempt
+def snippets_detail(request, pk):
+  try:
+    snippet = Snippet.objects.get(id=pk)
+  except:
+    return HttpResponse(status=404)
+
+  if request.method == "GET":
+    serializer = SnippetSerializer(snippet)
+    return JsonResponse(serializer.data)
+
+  if request.method == "PUT":
+    data = JSONParser.parse(request)
+    serializer= SnippetSerializer(snippet, data=data)
+    if serializer.is_valid():
+      serializer.save()
+      return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.data, status=400)
+
+  if request.method == "DELETE":
+    snippet.delete()
+    return HttpResponse(status=204)    
+
+
+
