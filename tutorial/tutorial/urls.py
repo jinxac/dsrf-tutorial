2e0f18fe21acc5385691f49926aaeab95111bfe8
salujaharkirat <saluja.harkirat@gmail.com>
2020-08-28 17:06:54 +0530

    step 4 drf tutorial

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -13,14 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('snippets.urls'))
+]
+
+urlpatterns += [
+    path('test-auth/', include('rest_framework.urls')),
 ]
 
 
