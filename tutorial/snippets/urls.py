2c3437229f1a38c6d62d90507e8369574bf29e16
salujaharkirat <saluja.harkirat@gmail.com>
2020-08-28 16:09:56 +0530

    step-3

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from snippets import views
 
 urlpatterns = [
-  path('snippets/', views.snippets_list),
-  path('snippets/<int:pk>/', views.snippets_detail)
+  path('snippets/', views.SnippetList.as_view()),
+  path('snippets/<int:pk>/', views.SnippetDetail.as_view())
 ]
