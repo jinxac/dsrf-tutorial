e78c2d3651023df28637421c474b12ddc29e1605
salujaharkirat <saluja.harkirat@gmail.com>
2020-08-28 16:20:32 +0530

    Generic views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,46 +6,19 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class SnippetList(
-  generics.GenericAPIView,
-  mixins.ListModelMixin,
-  mixins.CreateModelMixin):
+class SnippetList(generics.ListCreateAPIView):
 
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
 
-  def get(self, *args, **kwargs):
-    return self.list(self, *args, **kwargs)
 
-  def post(self, *args, **kwargs):
-    return self.create(self, *args, **kwargs)
-
-
-class SnippetDetail(
-  generics.GenericAPIView,
-  mixins.RetrieveModelMixin,
-  mixins.UpdateModelMixin,
-  mixins.DestroyModelMixin):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
-
-  def get(self, request, *args, **kwargs):
-    return self.retrieve(request, *args, **kwargs)
-
-  def put(self, request, *args, **kwargs):
-    return self.update(request, *args, **kwargs)
-
-  def delete(self, request, *args, **kwargs):
-    return self.destroy(request, *args, **kwargs)
 
 
 
